2e6a644d35610db585da22f4d317310170db2146
danghaian168@gmail.com <danghaian168@gmail.com@432929e3-113a-69ff-6cb3-855d194fc3e0>
2014-11-03 11:19:47 +0000

    # service accéléro : continue impl

--- projet_bato/src/accelero.c
+++ projet_bato/src/accelero.c
@@ -14,7 +14,7 @@ void accelero_init(void) {
 	/* Clock config */
 	// ADCCLK = PCLK2/4 
 	RCC_ADCCLKConfig(RCC_PCLK2_Div4);
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_ADC2 |
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2 |
                          RCC_APB2Periph_GPIOC, ENABLE);
 	
 	/* GPIO Config */
@@ -28,26 +28,21 @@ void accelero_init(void) {
 	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
 	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Left;
-	ADC_InitStructure.ADC_NbrOfChannel = 2;
+	ADC_InitStructure.ADC_NbrOfChannel = 1;
 	ADC_Init(ADC2, &ADC_InitStructure);
 	
 	/* ADC2 regular channels configuration */ 
   ADC_RegularChannelConfig(ADC2, ADC_Channel_10, 1, ADC_SampleTime);
-  ADC_RegularChannelConfig(ADC2, ADC_Channel_11, 2, ADC_SampleTime);
+  //ADC_RegularChannelConfig(ADC2, ADC_Channel_11, 2, ADC_SampleTime);
 	
 	/* Start ADC (AD_ON) */
 	ADC_Cmd(ADC2, ENABLE);
 	
-	/* Enable ADC1 reset calibration register */   
-  ADC_ResetCalibration(ADC2);
-  /* Check the end of ADC1 reset calibration register */
-  while(ADC_GetResetCalibrationStatus(ADC2));
 }
 
 int accelero_getAngle(void) {
-	/* Start ADC1 calibration */
-  ADC_StartCalibration(ADC2);
-  /* Check the end of ADC1 calibration */
-  while(ADC_GetCalibrationStatus(ADC2));
+	ADC_SoftwareStartConvCmd(ADC2, ENABLE);
+	while (!ADC_GetFlagStatus(ADC2, ADC_FLAG_EOC));
+	ADC_ClearFlag(ADC2, ADC_FLAG_STRT);
 	return ADC_GetConversionValue(ADC2);
 }
--- projet_bato/src/main.c
+++ projet_bato/src/main.c
@@ -5,12 +5,16 @@
 
 //____ registres de périphériques du stm_32
 #include "stm32f10x.h"
+#include "accelero.h"
 
 int test_accelero(void); 
-
+int capteur_adc;
 int main (void)
-{
-	test_accelero();
+{ 
+	accelero_init();
+	while (1) {
+		capteur_adc = accelero_getAngle();
+	}
 	return 0;
 }
 
